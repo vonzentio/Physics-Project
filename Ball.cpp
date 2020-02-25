2c10e276e8b5b328d5cb59f05bf1f15e48174f62
OliverEliardsson <50369007+OliverEliardsson@users.noreply.github.com>
2020-02-25 16:12:37 +0100

    Add files via upload

--- Ball.cpp
+++ Ball.cpp
@@ -1,47 +1,85 @@
 #include "Ball.h"
 
-Ball::Ball()
-	:m_velocity(0.f, 0.f), m_position(0.f, 0.f), m_radius(1.f), m_mass(1.f)
+Ball::Ball(float radius, sf::Color color)
+	:radius(radius)
 {
-}
-
-Ball::Ball(vec2_t position, vec2_t velocity, float radius, float mass)
-	:m_position(position), m_velocity(velocity), m_radius(radius), m_mass(mass), 
-	m_gravity(DEFAULT_GRAVITY)
-{
-	this->m_sphere.setRadius(radius);
-	this->m_sphere.setOrigin(sf::Vector2f(radius, radius));
-	this->m_sphere.setPosition(sf::Vector2f(position.x, position.y));
-}
-
-Ball::~Ball()
-{
-}
-
-void Ball::setPosition(vec2_t position)
-{
-}
-
-void Ball::setVelocity(vec2_t position)
-{
-}
-
-void Ball::update(float deltaTime)
-{
+	this->shape = new sf::CircleShape(radius);
+	this->shape->setFillColor(color);
 
 }
 
-vec2_t Ball::getPosition()
+void Ball::shoot(sf::Vector2i startPos, sf::Vector2f vel)
 {
-	return this->m_position;
+	this->started = true;
+	this->startVelocity = vel;
+	this->currentVelocity = startVelocity;
+	this->startPos = startPos;
+
 }
 
-vec2_t Ball::getVelocity()
+void Ball::update(float dt, sf::Vector2i cursorPos )
 {
-	return this->m_velocity;
+	static int dotDelay = 0;
+	static float timeSinceStart = 0.f;
+
+	if (started)
+	{
+		
+		this->launchAngle = launchAngle * PI / 180.0;
+		dotDelay++;
+		
+
+		float X = this->currentPos.x + this->startVelocity.x * cos(launchAngle) * dt;
+		float Y = this->currentPos.y + this->currentVelocity.y * sin(launchAngle) * dt - ((GRAVITY * pow(dt, 2)) / 2.0f); ;
+		
+
+		this->currentPos = sf::Vector2f(X, Y);
+		this->shape->setPosition(this->currentPos);
+
+		this->currentVelocity.y = currentVelocity.y + startVelocity.y * sin(this->launchAngle) - GRAVITY * dt;
+
+		vec2_t temp(currentVelocity.x, currentVelocity.y);
+		temp.normalize();
+		this->currentVelocity = sf::Vector2f(temp.x, temp.y);
+
+		this->launchAngle = atan(this->currentVelocity.y / currentVelocity.x);
+
+
+		if (dotDelay == dotDensity)
+		{
+			if (dotIndex == dotBufferCount)
+				dotIndex = 0;
+
+			sf::CircleShape dot(dotRadius);
+			dot.setFillColor(sf::Color::Green);
+			dot.setPosition(this->currentPos.x + radius, currentPos.y + radius);
+
+			this->dots[dotIndex++] = dot;
+			
+			dotDelay = 0;
+		}
+
+	}
+	else		//Follows cursor Position
+	{
+		this->shape->setPosition((float)cursorPos.x - this->radius, (float)cursorPos.y -this->radius);
+	}
+
+
 }
 
 void Ball::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	target.draw(this->m_sphere);
+
+	target.draw(*this->shape);
+	for (int i = 0; i < dotBufferCount; i++)
+		target.draw(dots[i]);
+
+
+}
+
+Ball::~Ball()
+{
+	delete this->shape;
+	delete[] this->dots;
 }
